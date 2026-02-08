ddeee4f487a7c1c5e18484d70ee460b40956f994
Sebastian Kootz <skxxtz@gmail.com>
2026-02-08 20:43:12 +0100

    chore: cargo fmt

--- src/launcher/web_launcher.rs
+++ src/launcher/web_launcher.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct WebLauncher {
-    #[serde(rename="search_engine")]
+    #[serde(rename = "search_engine")]
     pub engine: String,
     pub browser: Option<String>,
 }
